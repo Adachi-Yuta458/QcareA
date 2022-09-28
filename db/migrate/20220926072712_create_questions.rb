45775713853e62c513463360b7d0b1fe78a82daf
YutaAdachi <yuta.adachi@luxiar.com>
2022-09-28 14:41:23 +0900

    statusのdefault値を設定

--- db/migrate/20220926072712_create_questions.rb
+++ db/migrate/20220926072712_create_questions.rb
@@ -5,7 +5,7 @@ class CreateQuestions < ActiveRecord::Migration[6.1]
     create_table :questions do |t|
       t.string :title, null: false
       t.text :content, null: false
-      t.integer :status, null: false
+      t.integer :status, null: false, default: 0
       t.references :local_government, null: false, foreign_key: true
 
       t.timestamps
