96ec6fdd76f66397dce2280e1db878f0bb9e11e3
YutaAdachi <yuta.adachi@luxiar.com>
2022-09-28 10:12:13 +0900

    カラム名をbodyからcontent に変更、回答ステータスを保持するカラムstatusを追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,8 @@
 
 class Question < ApplicationRecord
   belongs_to :local_government
+
+  VALID_STATUSES = ['wait', 'working', 'completed']
+
+  validates :status, inclusion: { in: VALID_STATUSES}
 end
--- db/migrate/20220926072712_create_questions.rb
+++ db/migrate/20220926072712_create_questions.rb
@@ -4,7 +4,8 @@ class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
       t.string :title, null: false
-      t.text :body, null: false
+      t.text :content, null: false
+      t.string :status, null: false
       t.references :local_government, null: false, foreign_key: true
 
       t.timestamps
