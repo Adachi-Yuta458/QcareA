78e66340ded7f2e2ca15897dc45a80da295379d4
YutaAdachi <yuta.adachi@luxiar.com>
2022-09-29 13:54:54 +0900

    Revert "[役所] 質問一覧ページ作成"
    This reverts commit 2abf41f658afbe61a04628cd206105e923ddbec7.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :questions
-  end
   root to: 'questions#index'
-  #resources :questions
+  resources :questions
 end
