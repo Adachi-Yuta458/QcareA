45fb0812d06902845c65dde977756c5c0909d4c2
YutaAdachi <yuta.adachi@luxiar.com>
2022-10-12 11:44:12 +0900

    介護職員テーブル作成

---
+++ db/migrate/20221012023825_create_staffs.rb
@@ -0,0 +1,13 @@
+class CreateStaffs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :staffs do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+      t.integer :nursing_facility_id, null: false
+      t.string :password_digest
+
+      t.timestamps
+      t.index :email, unique: true
+    end
+  end
+end
