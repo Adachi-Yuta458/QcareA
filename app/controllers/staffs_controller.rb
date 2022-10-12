b817ae325af9d11b25fcc342253ce6dd12b076be
YutaAdachi <yuta.adachi@luxiar.com>
2022-10-12 18:10:30 +0900

    編集機能の実装

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -21,6 +21,20 @@ class StaffsController < ApplicationController
     @staff = Staff.find(params[:id])
   end
 
+  def edit
+    @staff = Staff.find(params[:id])
+  end
+
+  def update
+    @staff = Staff.find(params[:id])
+
+    if @staff.update(staff_params)
+      redirect_to staff_url(@staff), notice: "職員「#{@staff.name}」を更新しました。"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def staff_params
