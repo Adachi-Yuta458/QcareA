3762778635cd7504fe9a4bfcc8b8301db952c3b6
YutaAdachi <yuta.adachi@luxiar.com>
2022-10-13 15:55:01 +0900

    ログイン機能の実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    staff = Staff.find_by(email: session_params[:email])
+
+    if staff&.authenticate(session_params[:password])
+      session[:staff_id] = staff.id
+      redirect_to root_url, notice: 'ログインしました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
