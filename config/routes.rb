49d6f704d6e8083917c520a03731548481324606
YutaAdachi <yuta.adachi@luxiar.com>
2022-10-13 15:53:57 +0900

    ログインフォームの実装

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
   root to: 'questions#index'
   resources :questions
   resources :staffs
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
